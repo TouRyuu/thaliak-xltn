a288df5efb8e77527d8746eca7d002fc16dac4af
TouRyuu <ceg9498@gmail.com>
2019-06-12 15:25:01 -0400

    #16 more tags replaced

--- src/lib/Actions.tsx
+++ src/lib/Actions.tsx
@@ -52,65 +52,61 @@ export default abstract class Actions extends Component<AppState,AppState> {
   GetText(){
     // Fetch new quest text from XIVAPI
     let page:number = -1;
-    let item:number = -1;
-    let dialogueID:number = -1;
+    let qIndex:number = -1;
+    let dIndex:number = -1;
     let name = "";
     
     // get a random PAGE number
     if(this.state.pages !== undefined){
       page = this.RNG(this.state.pages);
+      page++;
     }
     
     // Ask the API for the random PAGE's list
     xiv.data.list(`Quest?page=${page}`).then((response:any) => {
       do {
         // get a random ITEM number based on how many results are on the page
-        item = this.RNG(response.Pagination.Results);
+        qIndex = this.RNG(response.Pagination.Results);
         
         // check the name value from the list to remove invalid possibilities
-        name = response.Results[item].Name;
+        name = response.Results[qIndex].Name;
         if(name === "Testdfghjkl;"){
           name = "";
         }
       }while(name === "")
 
       // once the name value is OK, update `item` to store the ID
-      item = response.Results[item].ID;
+      qIndex = response.Results[qIndex].ID;
 
       // use the item ID to get details about that Quest
-      xiv.data.get("Quest",item).then((response:any) => {
+      xiv.data.get("Quest",qIndex).then((response:any) => {
         let tempDE = response.TextData_de.Dialogue;
         let tempEN = response.TextData_en.Dialogue;
         let tempFR = response.TextData_fr.Dialogue;
         let tempJP = response.TextData_ja.Dialogue;
         
         // get a random DIALOGUE number
-        let numDialogue = tempEN.length;
-        dialogueID = this.RNG(numDialogue);
+        dIndex = this.RNG(tempEN.length);
 
-        tempDE = tempDE[dialogueID].Text;
-        tempEN = tempEN[dialogueID].Text;
-        tempFR = tempFR[dialogueID].Text;
-        tempJP = tempJP[dialogueID].Text;
+        tempDE = tempDE[dIndex].Text;
+        tempEN = tempEN[dIndex].Text;
+        tempFR = tempFR[dIndex].Text;
+        tempJP = tempJP[dIndex].Text;
 
         if(/>/.test(tempDE)){
-          //console.log("DE string contains >")
           tempDE = this.ReplaceCode(tempDE);
         }
         if(/>/.test(tempEN)){
-          //console.log("EN string contains >")
           tempEN = this.ReplaceCode(tempEN);
         }
         if(/>/.test(tempFR)){
-          //console.log("FR string contains >")
           tempFR = this.ReplaceCode(tempFR);
         }
         if(/>/.test(tempJP)){
-          //console.log("JP string contains >")
           tempJP = this.ReplaceCode(tempJP);
         }
 
-        // assign strings from the dialog number to state
+        // assign strings to state & set haveText to true
         this.setState({
           text: {
             DE: tempDE,
@@ -123,7 +119,7 @@ export default abstract class Actions extends Component<AppState,AppState> {
         
       // close out individual quest retrieval
       }).catch((error:Error) => {
-        console.error(`Error in getting item ${item}: `, error)
+        console.error(`Error in getting item ${qIndex}: `, error)
       })
     // close out the list retrieval
     }).catch((error:Error) => {
@@ -137,146 +133,98 @@ export default abstract class Actions extends Component<AppState,AppState> {
   }
 
   ReplaceCode(toChange:string):string{
-    // basic replacements
+    // basic replacements/removals
     toChange = toChange.replace(/<SoftHyphen\/>/g, "-");
     toChange = toChange.replace(/<Indent\/>/g, "");
-    if(/Emphasis/.test(toChange)){
-      // For now I'm just removing Emphasis tags
-      // In the future I'd like to replace with bold/italic text
-      toChange = toChange.replace(/<Emphasis>/g, "");
-      toChange = toChange.replace(/<\/Emphasis>/g, "");
-    }
+    toChange = toChange.replace(/<.?Emphasis>/g, "");
+    toChange = toChange.replace(/<UIForeground>.?[0-9]*<\/UIForeground>/g, "");
 
-    // Fix display of gendered text
-    if(/PlayerParameter\(4\)/.test(toChange)){
-      toChange = this.Gender(toChange);
-    }
+    if(/PlayerParameter/.test(toChange)){
+      // Fix display of gendered text
+      toChange = toChange.replace(/<If\(PlayerParameter\(4\)\)>/g,"<If>");
 
-    // Fix display of racial text
-    if(/PlayerParameter\(71\)/.test(toChange)){
-      toChange = this.Gender(toChange);
+      // Fix display of time-relative text
+      // /<If\(LessThan\(PlayerParameter\(11\),12\)\)>/
+      toChange = toChange.replace(/<If\(LessThan\(PlayerParameter\(11\),\d{1,3}\)\)>/g, "<If>")
+      
+      // Fix display of PlayerParameter(68), which references current job
+      toChange = toChange.replace(/<If.{0,8}\(PlayerParameter\(68\).{0,8}/, "<If>")
+      // This handles any locations of self-closing tags
+      toChange = toChange.replace(/<.{9,16}PlayerParameter\(68\).{0,8}>/g,"[[class/job]]")
+
+      // Fix display of racial text
+      toChange = toChange.replace(/<If.{0,6}\(PlayerParameter\(71\).{0,4}\)>/g,"<If>");
     }
     
-    // change character name to placeholder
+    // change character name code to [[character name]]
+    // cNameS is for when the code would display a character's full name
     let cNameS = /<Highlight>ObjectParameter\(1\)<\/Highlight>/g;
-    let cNameL = /<Split\(<Highlight>ObjectParameter\(.?\)<\/Highlight>.{2,6}\)\/>/g;
+    // cNameL is for when the code would display first OR last name
+    let cNameL = /<Split\(<Highlight>ObjectParameter\(1\)<\/Highlight>.{2,6}\)\/>/;
     if(cNameS.test(toChange)){
       toChange = toChange.replace(cNameL, "[[character name]]");
       toChange = toChange.replace(cNameS, "[[character name]]");
     }
 
     // Fix display of things that reference other sheets
-    if(/Item/.test(toChange)){
-      toChange = toChange.replace(/<.{6,14}Item.{6,18}\/>/g,"[[item]]");
-    }
-    if(/EObj/.test(toChange)){
+    if(/<Sheet/.test(toChange)){
       toChange = toChange.replace(/<.{6,9}EObj.{6,18}\/>/g,"[[Event Object]]");
-    }
-    if(/BNpcName/.test(toChange)){
       toChange = toChange.replace(/<.{6,9}BNpcName.{6,18}\/>/g, "[[monster]]");
+      // This first Regex allows more text in front than the other two
+      // Because I want it to also catch EventItem
+      toChange = toChange.replace(/<.{6,14}Item.{6,18}\/>/g,"[[item]]");
+      // Sometimes gendered language shows up for item/monster names; prepare for fix
+      toChange = toChange.replace(/<If\(\[\[.{1-13}\]\]\)>/, "<If>");
     }
 
-    // Fix display of PlayerParameter(68), which references current job
-    if(/PlayerParameter\(68\)/.test(toChange)){
-      // this handles any locations of self-closing tags
-      toChange = toChange.replace(/<.{9,16}PlayerParameter\(68\).{0,8}>/g,"[[class/job]]")
+    // Fix any If/Else blocks that were prepared by previous replaces
+    if(/<If>/.test(toChange)){
+      // Replaces for nested If blocks so they also look good
+      // This was built specifically for time checks, but may also work for Class/Job
+      toChange = toChange.replace(/<If><If>/g, "(");
+      toChange = toChange.replace(/<\/If><Else\/><If>/g, "/");
+      toChange = toChange.replace(/<\/If><\/If>/g, ")");
+      // Opening for when true is empty
+      toChange = toChange.replace(/<If><Else\/>/g, "(");
+      // Closing for when false is empty
+      toChange = toChange.replace(/<Else\/><\/If>/g, ")");
+      // Replace any remaining individual tags
+      toChange = toChange.replace(/<If>/g, "(");
+      toChange = toChange.replace(/<Else\/>/g, "/");
+      toChange = toChange.replace(/<\/If>/g, ")");
     }
 
-    // remove wrappers such as Clickable and Split
-    toChange = toChange.replace(/<Split\(/g, "");
-    toChange = toChange.replace(/<Clickable\(/g, "");
+    // check if there's still angle braces in the text
+    if(/</.test(toChange) || />/.test(toChange)){
+      // remove leftover wrappers such as Clickable and Split
+      // only removing the front here, as sometimes the end is grabbed by earlier replaces
+      toChange = toChange.replace(/<Split\(/g, "");
+      toChange = toChange.replace(/<Clickable\(/g, "");
 
-    // remove stray tag closers
-    // This should be uncommented once I'm happy with everything else
-    //toChange = toChange.replace(/\/>/g, "")
+      // remove stray tag closers here
+      // This should be uncommented once I'm happy with everything else
+      // If I put it in before everything else is done it will just be confusing
+      //toChange = toChange.replace(/\/>/g, "")
+    }
 
     // send the new text back
-    let changed = toChange;
-    return changed;
-  }
-
-  // This function handles gendered & racial text,
-  // as designated in the text by PlayerParameter(4) & PlayerParameter(71)
-  Gender(fix:string):string {
-    // get number of times PP4 is used
-    let num = (fix.split(/\(PlayerParameter\(.{0,2}\)/g).length -1);
-
-    // cycle through the string that many times
-    for(let i = 0; i < num; i++){
-      // sections with only TRUE option
-      if(/<Else\/><\/If>/.test(fix)){
-        //<If.{0,6}\(PlayerParameter\(.{0,2}\).{0,4}\)>.{0,20}<Else\/>.{0,20}<\/If>
-        fix = fix.replace(/<If.{0,6}\(PlayerParameter\(.{0,2}\).{0,4}\)>.{1,20}<Else\/><\/If>/, (x)=>{
-          x = x.replace(/<If.{0,6}\(PlayerParameter\(.{0,2}\).{0,4}\)>/, "(");
-          x = x.replace(/<Else\/><\/If>/, ")");
-          return x;
-        });
-      // sections with only FALSE option
-      } else if(/<If.{0,6}\(PlayerParameter\(.{0,2}\).{0,4}\)><Else\/>/.test(fix)){
-        fix = fix.replace(/<If.{0,6}\(PlayerParameter\(.{0,2}\).{0,4}\)><Else\/>.{1,20}<\/If>/, (x)=>{
-          x = x.replace(/<If.{0,6}\(PlayerParameter\(.{0,2}\).{0,4}\)><Else\/>/, "(");
-          x = x.replace(/<\/If>/, ")");
-          return x;
-        });
-      // sections with both TRUE and FALSE options
-      } else {
-        fix = fix.replace(/<If.{0,6}\(PlayerParameter\(.{0,2}\).{0,4}\)>.{1,20}<Else\/>.{1,20}<\/If>/, (x)=>{
-          x = x.replace(/<If.{0,6}\(PlayerParameter\(.{0,2}\).{0,4}\)>/, "(");
-          x = x.replace(/<Else\/>/, "/");
-          x = x.replace(/<\/If>/, ")");
-          return x;
-        });
-      }
-    }
-
-    return fix;
+    return toChange;
   }
 }
 
 /*
 REMAINING PROBLEMS
 
-** PP(11) is time of day
-A pleasant 
-<If(LessThan(PlayerParameter(11),12))>
-  <If(LessThan(PlayerParameter(11),4))>
-    evening
-  <Else/>
-    morning
-  </If>
-<Else/>
-  <If(LessThan(PlayerParameter(11),17))>
-    afternoon
-  <Else/>
-    evening
-  </If>
-</If>
- to you, (Mistress/Master)
-*/
+Thank you, <If(GreaterThan(PlayerParameter(52),0))><Sheet(GCRankLimsaMaleText,PlayerParameter(52),8)/><Else/></If><If(GreaterThan(PlayerParameter(53),0))><Sheet(GCRankGridaniaMaleText,PlayerParameter(53),8)/><Else/></If><If(GreaterThan(PlayerParameter(54),0))><Sheet(GCRankUldahMaleText,PlayerParameter(54),8)/><Else/></If>)/> [[character name]]─this will go a long way to helping the nation recover from the ravages of war!
+Un grand merci pour votre col-la-bo-ra-tion, <If(GreaterThan(PlayerParameter(52),0))><If(GreaterThan(PlayerParameter(52),0))>(<Sheet(GCRankLimsaFemaleText,PlayerParameter(52),0)/>/<Sheet(GCRankLimsaMaleText,PlayerParameter(52),0)/>)) [[character name]])<If(GreaterThan(PlayerParameter(53),0))><If(GreaterThan(PlayerParameter(53),0))>(<Sheet(GCRankGridaniaFemaleText,PlayerParameter(53),0)/>/<Sheet(GCRankGridaniaMaleText,PlayerParameter(53),0)/>)) [[character name]], ,2)/>)<If(GreaterThan(PlayerParameter(54),0))><If(GreaterThan(PlayerParameter(54),0))>(<Sheet(GCRankUldahFemaleText,PlayerParameter(54),0)/>/<Sheet(GCRankUldahMaleText,PlayerParameter(54),0)/>)) [[character name]], ,2)/>)!
 
-/*
-SHOULD BE FIXED
+** PlayerParameter(70) is...?
+<Switch(PlayerParameter(70))><Case(1)>The building you seek stands in the southern part of the city, now called New Gridania</Case><Case(2)>I daresay you know it well from your wanderings in New Gridania</Case><Case(3)>The building you seek stands in the southern part of the city, now called New Gridania</Case></Switch>. Give your name to the personnel officer there, and he will guide you through the formalities.
 
-[(GERMAN) no feminine option for gendered text]
-
-[[character name]], ,1)/>
-
-Si l'aventuri(ère<Else/>er</If> est aussi gentil(le) qu'(elle/il) en a l'air, (elle/il) ira botter les fesses de ces fichus engins! <If(PlayerParameter(4))>Elle/Il) n'a qu'à en casser trois près de L'Arkhitékton, ça embêtera bien la Main indigo!
-Si l'aventuri<If(PlayerParameter(4))>ère<Else/>er</If> est aussi gentil<If(PlayerParameter(4))>le<Else/></If> qu'<If(PlayerParameter(4))>elle<Else/>il</If> en a l'air, <If(PlayerParameter(4))>elle<Else/>il</If> ira botter les fesses de ces fichus engins! <If(PlayerParameter(4))>Elle<Else/>Il</If> n'a qu'à en casser trois près de L'Arkhitékton, ça embêtera bien la Main indigo!
-
-** PP(71) is character race
-Des Roegadyns comme <If(Equal(PlayerParameter(71),5))>vous et moi<Else/>moi</If> auront utilisé cette pioche.
-
-important news from you, <Split(<Highlight>ObjectParameter(1)</Highlight>, ,1)/>! Lady Leveva and Quimperain
-Oh, vous êtes <Highlight>ObjectParameter(1)</Highlight>, (la/le) célèbre Hériti(ère/er) de la Septième Aube!
-
-「<Sheet(Item,5333,0)/>」は、ふわふわの「<Sheet(Item,5341,0)/>」をつむいで作る糸よ。
-悪さをする「<Sheet(BNpcName,3495,0)/>」を倒してほしいんだ。
-
-mir [[item]] zu. <Clickable([[item]] dafür
-<Clickable([[item]])/>
+** Massive class/job blocks
 
 */
+
 
 // la gloire de la grande frairie
 // his whiskers
@@ -284,8 +232,9 @@ mir [[item]] zu. <Clickable([[item]] dafür
 // おおなまずのまにまに
 
 /*
+Deletion placeholders.
 
 （★未使用／削除予定★）
-
+（カット）
 DE: Leer / EN: Textless / JP: [kuu]raberu
 */
