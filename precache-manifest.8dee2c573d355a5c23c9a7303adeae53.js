34ecd08c499f914fad9513776c723ebe848b61fd
Emily Gagne <ceg9498@gmail.com>
2022-11-08 16:36:14 -0500

    Updates

---
+++ precache-manifest.8dee2c573d355a5c23c9a7303adeae53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923358ceef85b2ed5a53170895e8ec2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eec6b5d4b996fb9dd74",
+    "url": "/static/css/main.7087c0ed.chunk.css"
+  },
+  {
+    "revision": "a6b496c942e252c63eb6",
+    "url": "/static/js/2.1f3979a6.chunk.js"
+  },
+  {
+    "revision": "1eec6b5d4b996fb9dd74",
+    "url": "/static/js/main.b1efb92a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0524f5f047c4f858371c789372f8d010",
+    "url": "/static/media/background.0524f5f0.png"
+  }
+]);
